d712d3fb1b5bd7e282ec974a4f5d15726aca5c4a
ritubjoshi28 <102813170+ritubjoshi28@users.noreply.github.com>
2022-04-13 16:57:55 -0500

    add day and time

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
+function rightNow() {
+  let now = new Date();
+  let days = ["Sunday", "Monday", "Tuesday", "Wedensday", "Friday", "saturday"];
+  let currentDay = days[now.getDay()];
+  let hours = now.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let h6 = document.querySelector("h6");
+  h6.innerHTML = `${currentDay} ${hours}:${minutes}`;
+}
+rightNow();
+
 function formateDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+  let days = ["Mon", "Tue", "Wed", "Thur", "Fri", "Sat", "Sun"];
   return days[day];
 }
 
@@ -51,7 +68,7 @@ function getForce(coordinates) {
 }
 
 function showCity(response) {
-  console.log(response.data);
+  //console.log(response.data);
   document.querySelector("#dream").innerHTML = response.data.name;
   document.querySelector("#celcius").innerHTML = `${Math.round(
     response.data.main.temp
@@ -72,14 +89,17 @@ function showCity(response) {
   displaycelcius = response.data.main.temp;
   getForce(response.data.coord);
 }
-function showTemperature(event) {
-  event.preventDefault();
+function search(city) {
   let apiKey = "dd148c52602d8cdd859f994ef40ed094";
-  let searchInput = document.querySelector("#look-input");
-  let city = searchInput.value;
   let units = "metric";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
   axios.get(apiUrl).then(showCity);
+}
+function showTemperature(event) {
+  event.preventDefault();
+
+  let searchInput = document.querySelector("#look-input");
+  search(searchInput.value);
 }
 
 function showFereniteTemperature(event) {
@@ -95,16 +115,13 @@ function displaycelTemperature(event) {
 }
 let displaycelcius = null;
 
-let form = document.querySelector("#search-form");
-form.addEventListener("submit", showTemperature);
-
 function showPosition(position) {
   let lat = position.coords.latitude;
   let lon = position.coords.longitude;
   let geoApiKey = "dd148c52602d8cdd859f994ef40ed094";
   let unit = "metric";
   let geoApiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${geoApiKey}&units=${unit}`;
-  let say = alert(`your latitude is${position.coords.latitude}`);
+  //let say = alert(`your latitude is${position.coords.latitude}`);
   console.log(position.coords.latitude);
   console.log(position.coords.latitude);
   axios.get(geoApiUrl).then(showCity);
@@ -113,11 +130,17 @@ function getCurrentPosition() {
   navigator.geolocation.getCurrentPosition(showPosition);
 }
 
-let button = document.querySelector("button");
-button.addEventListener("click", getCurrentPosition, displayForecast);
+let button = document.querySelector("#pin");
+button.addEventListener("click", getCurrentPosition);
+button.addEventListener("click", displayForecast);
 
 let fereiniteElement = document.querySelector("#farenheit-link");
 fereiniteElement.addEventListener("click", showFereniteTemperature);
 
 let cellElement = document.querySelector("#merits");
 cellElement.addEventListener("click", displaycelTemperature);
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", showTemperature);
+
+search("Apple valley");
